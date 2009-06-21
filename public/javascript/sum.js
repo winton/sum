ed8c8f5c2132274e53239df683a44a301c8495fe
winton <mail@wintoni.us>
2009-06-21 12:23:45 -0700

    Adding facebox dialogs

--- public/javascript/sum.js
+++ public/javascript/sum.js
@@ -2,4 +2,8 @@ jQuery(function($) {
   $('form').bind('submit', function() {
     $('#timezone_offset').attr('value', -(new Date()).getTimezoneOffset() * 60);
   });
+  $('*[rel*=facebox]').facebox({
+    closeImage: '/image/facebox/closelabel.gif',
+    loadingImage: '/image/facebox/loading.gif'
+  });
 });
