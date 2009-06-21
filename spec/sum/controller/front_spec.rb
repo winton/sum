b878d599f6d7b58c19bedf0df729e7798a961115
winton <mail@wintoni.us>
2009-06-21 02:07:14 -0700

    Good coverage, can start on daemon app now

--- spec/sum/controller/front_spec.rb
+++ spec/sum/controller/front_spec.rb
@@ -7,10 +7,72 @@ describe 'Front Controller' do
     Application
   end
   
-  describe '/' do
-    it "get responds ok" do
+  describe 'get /' do
+    
+    before(:each) do
       get '/'
+    end
+    
+    it "should respond ok" do
       last_response.should be_ok
+    end
+    
+    it "should contain instructions" do
+      last_response.body.include?("class='number'").should == true
+    end
+    
+    it "should contain a form" do
+      last_response.body.include?("<form").should == true
+    end
+  end
+  
+  describe 'post /new' do
+    describe 'valid' do
+      
+      before(:each) do
+        post '/new',
+          "user[email]" => "test@test.com",
+          "user[bills]" => "1000.02",
+          "user[income]" => "2500.54",
+          "user[savings]" => "500.02",
+          "user[timezone_offset]" => "-25200"
+      end
+    
+      it "should respond ok" do
+        last_response.should be_ok
+      end
+    
+      it "should succeed" do
+        last_response.body.include?("Success!").should == true
+      end
+    
+      it "should not contain a form" do
+        last_response.body.include?("<form").should == false
+      end
+    end
+    
+    describe 'invalid' do
+      
+      before(:each) do
+        post '/new',
+          "user[email]" => "",
+          "user[bills]" => "",
+          "user[income]" => "",
+          "user[savings]" => "",
+          "user[timezone_offset]" => ""
+      end
+    
+      it "should respond ok" do
+        last_response.should be_ok
+      end
+    
+      it "should not succeed" do
+        last_response.body.include?("Success!").should == false
+      end
+    
+      it "should contain a form" do
+        last_response.body.include?("<form").should == true
+      end
     end
   end
 end
