2590b9e18e1a127d80709ebee39fd845511f89c1
winton <mail@wintoni.us>
2009-06-28 23:13:41 -0700

    Sending email when user updates budget numbers

--- lib/sum/controller/new.rb
+++ lib/sum/controller/new.rb
@@ -4,6 +4,7 @@ Application.class_eval do
     @title = valid? ? 'Success!' : 'Fix mistakes'
     @title = [ 'New budget', @title ].compact.join(' - ')
     if @user = User.find_by_email(params[:user][:email])
+      @user.send_now = true
       @user.update_attributes(params[:user])
     else
       @user = User.create(params[:user])
