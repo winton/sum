33434711b39954c663e73ebd5f4e26d968f4c112
winton <mail@wintoni.us>
2009-07-19 03:36:52 -0700

    Error in more concise gem requiring

--- lib/sum.rb
+++ lib/sum.rb
@@ -1,11 +1,11 @@
 require 'rubygems'
 
 gems = [
-  'haml', '=2.0.9',
-  'sinatra', '=0.9.2',
-  'winton-active_wrapper', '=0.1.5',
-  'winton-fetcher', '=0.1.2',
-  'winton-secret_key', '=0.1.0'
+  [ 'haml', '=2.0.9' ],
+  [ 'sinatra', '=0.9.2' ],
+  [ 'winton-active_wrapper', '=0.1.5' ],
+  [ 'winton-fetcher', '=0.1.2' ],
+  [ 'winton-secret_key', '=0.1.0' ]
 ]
 
 gems.each do |name, version|
