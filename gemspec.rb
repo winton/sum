f39009bb9ecb21ef7cec1ebcd409f2acc9e9cb94
winton <mail@wintoni.us>
2009-07-19 16:23:44 -0700

    Version 0.1.0

--- gemspec.rb
+++ gemspec.rb
@@ -1,5 +1,19 @@
 GEM_NAME = 'sum'
-GEM_FILES = FileList['**/*'] - FileList['coverage', 'coverage/**/*', 'pkg', 'pkg/**/*']
+GEM_FILES = FileList['**/*'] - FileList[
+  '*.gem',
+  '*.log',
+  'config/database.yml',
+  'config/deploy.rb',
+  'config/mail.yml',
+  'coverage',
+  'coverage/**/*',
+  'log',
+  'log/**/*',
+  'pkg',
+  'pkg/**/*',
+  'tmp',
+  'tmp/**/*'
+]
 GEM_SPEC = Gem::Specification.new do |s|
   # == CONFIGURE ==
   s.author = "Winton Welsh"
@@ -21,12 +35,11 @@ GEM_SPEC = Gem::Specification.new do |s|
   s.add_dependency('winton-externals', '=1.0.2')
   s.add_dependency('winton-fetcher', '=0.1.2')
   s.add_dependency('winton-secret_key', '=0.1.0')
-  s.executables << GEM_NAME
   s.extra_rdoc_files = [ "README.markdown" ]
   s.files = GEM_FILES.to_a
   s.has_rdoc = false
   s.name = GEM_NAME
   s.platform = Gem::Platform::RUBY
   s.require_path = "lib"
-  s.version = "0.0.0"
+  s.version = "0.1.0"
 end
